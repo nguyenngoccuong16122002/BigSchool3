a424a143cdbb953befcf32d25d4b010b562d182d
ely.nguyen <ely.nguyen@tpfgroup.cc>
2023-03-17 23:33:25 +0700

    Add project files.

---
+++ BigSchool3/Migrations/202303171545411_AddNameColumnToApplictionUser.cs
@@ -0,0 +1,18 @@
+﻿namespace BigSchool3.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class AddNameColumnToApplictionUser : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.AspNetUsers", "Name", c => c.String(nullable: false, maxLength: 255));
+        }
+        
+        public override void Down()
+        {
+            DropColumn("dbo.AspNetUsers", "Name");
+        }
+    }
+}
---
+++ BigSchool3/ViewModels/CourseViewModel.cs
@@ -0,0 +1,31 @@
+﻿using BigSchool3.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Globalization;
+using System.Linq;
+using System.Web;
+
+namespace BigSchool3.ViewModels
+{
+    public class CourseViewModel
+    {
+        [Required(ErrorMessage = "Bắt buộc nhập")]
+        public string Place { get; set; }
+        [Required(ErrorMessage = "Bắt buộc nhập")]
+        [FutureDate]
+        public string Date { get; set; }
+        [Required(ErrorMessage = "Bắt buộc nhập")]
+        [ValidTime]
+        public string Time { get; set; }
+        [Required(ErrorMessage = "Bắt buộc nhập")]
+        public byte Category { get; set; }
+        public IEnumerable<Category> Categoies { get; set; }
+        public DateTime GetDateTime()
+        {
+            string dateInString = string.Format("{0} {1}", Date, Time);
+
+            return DateTime.ParseExact(dateInString, "d/M/yyyy hh:mm", CultureInfo.InvariantCulture);
+        }
+    }
+}
